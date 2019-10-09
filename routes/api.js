57d02f464d9943c44b6ebc19b9ccc83648482b46
Gabriel Nance <gabriel.nance@selu.edu>
2019-10-09 12:16:12 -0500

    Added Get one req

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,13 @@ const mongoose = require('mongoose');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const bodyParser = require('body-parser');
+json = bodyParser.json();
 
 router.get('/', function(req, res){
     res.send('Hello World! ACM Website coming soon.');
 });
 //GETALL
-router.get('/member',function(req, res, next){
+router.get('/members',function(req, res, next){
     const MongoClient = require('mongodb').MongoClient;
     const uri = "mongodb+srv://jpeter:0nyx@acm-eb7i4.mongodb.net/test?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true,
@@ -27,20 +28,33 @@ router.get('/member',function(req, res, next){
         });
     });
 });
-
-// //GET
-// router.get('/member', function (req, res){
-//     res.send({type : 'GET'});
-// });
+//Get one
+router.get('/member', json, function(req, res, next){
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://jpeter:0nyx@acm-eb7i4.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true,
+        useUnifiedTopology: true
+    });    
+    client.connect(err => {
+        const collection = client.db("ACM").collection("Member");
+        var ObjectID = require("mongodb").ObjectID
+        collection.find({w_num: req.body.w_num}).toArray(function(error, result) {
+            if(error) {
+                return res.status(500).send(error);
+            }
+            res.send(result);
+        });
+    });
+});
 
 //add new member to db
-router.post('/member', function(req, res){
+router.post('/member', json, function(req, res){
     const first_name = req.first_name;
     const last_name = req.last_name;
     const email = req.email;
     const major = req.major;
     const w_num = req.w_num;
-    const signup_date = Date.now();
+    const signup_date = new Date();
     const password = req.password;
 
 const MongoClient = require('mongodb').MongoClient;
