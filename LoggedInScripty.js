120164c329c36adf310b79382ab798f93a5c8e7e
Lindsey Poche <lindsey.poche-2@selu.edu>
2019-11-26 12:54:33 -0600

    Revert "Merge branch 'working_craig' of https://github.com/clintonjwalker/cmps285-fall2019-section02-group1 into working_Lindsey"
    This reverts commit f2370d791b3e805e530a70dbe5ec1024069333a6, reversing
    changes made to 968e942232b8824e5a5061b4b3aaa17b805fdeea.

--- LoggedInScripty.js
+++ LoggedInScripty.js
@@ -1,7 +1,3 @@
-// import {router} from './routes/api';
-// var express = require('express')
-// var app = express()
-
 const app = document.getElementById('root')
 
 const container = document.createElement('div')
@@ -10,17 +6,15 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'http://localhost:3000/member?member=w4635876', true)
+
+request.open('GET', 'http://slu-acm.herokuapp.com/member', true)
 request.onload = function() {
-  // app.route('/member')
-  //   .get(function (req, res){})
   // Begin accessing JSON data here
   var memdat = JSON.parse(this.response)
-    memdat.forEach(Member => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
-      const h2 = document.createElement('h7')
+      const h2 = document.createElement('h2')
       h2.textContent = Member.first_name + " " + Member.last_name
 
       const info_e = document.createElement('p')
@@ -47,8 +41,6 @@ request.onload = function() {
       card.appendChild(info_s)
       card.appendChild(info_ed)
 
-    })
-  }
+}
 
-// })
 request.send()
