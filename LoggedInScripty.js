f4859c43429c17a85473489fc2f5894e964a811b
Craig Canepa <Craigcanepa10@gmail.com>
2019-11-26 12:37:28 -0600

    Why wasnt this commited sorry for the somewhat spam guys.

--- LoggedInScripty.js
+++ LoggedInScripty.js
@@ -1,3 +1,7 @@
+// import {router} from './routes/api';
+// var express = require('express')
+// var app = express()
+
 const app = document.getElementById('root')
 
 const container = document.createElement('div')
@@ -6,13 +10,17 @@ container.setAttribute('class', 'container')
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-
+request.open('GET', 'http://localhost:3000/member?member=w4635876', true)
+request.onload = function() {
+  // app.route('/member')
+  //   .get(function (req, res){})
   // Begin accessing JSON data here
   var memdat = JSON.parse(this.response)
+    memdat.forEach(Member => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
 
-      const h2 = document.createElement('h2')
+      const h2 = document.createElement('h7')
       h2.textContent = Member.first_name + " " + Member.last_name
 
       const info_e = document.createElement('p')
@@ -39,6 +47,8 @@ var request = new XMLHttpRequest()
       card.appendChild(info_s)
       card.appendChild(info_ed)
 
-}
+    })
+  }
 
+// })
 request.send()
